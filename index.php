25627ce1a4828e60b6f864a56502450def101817
Varesva <varesva.dev@gmail.com>
2025-02-19 20:59:14 +0100

    start

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>To Do List</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+	<table>
+		<thead>
+			<tr>
+				<th scope="col" id="col_todo">À faire</th>
+				<th scope="col" id="col_doing">En cours</th>
+				<th scope="col" id="col_done">Terminé</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td id="row_todo"></td>
+				<td id="row_doing"></td>
+				<td id="row_done"></td>
+			</tr>
+		</tbody>
+	</table>
+
+</body>
+
+</html>
